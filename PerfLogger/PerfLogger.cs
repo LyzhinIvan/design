b97abb41a47e74019bb2e3ab9f03d611450ef5f9
LyzhinIvan <lyzhin-ivan@yandex.ru>
2015-05-02 12:06:19 +0500

    PerfLogger has been implemented

---
+++ PerfLogger/PerfLogger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerfLogger
+{
+    public static class PerfLogger
+    {
+        public class StopWatcher : IDisposable
+        {
+            private Action<long> actionOnDispose;
+            private Stopwatch stopwatch;
+
+            public StopWatcher(Action<long> actionOnDispose)
+            {
+                this.actionOnDispose = actionOnDispose;
+                stopwatch = new Stopwatch();
+                stopwatch.Start();
+            }
+
+            public void Dispose()
+            {
+                stopwatch.Stop();
+                actionOnDispose(stopwatch.ElapsedMilliseconds);
+            }  
+        }
+
+        public static IDisposable Measure(Action<long> actionOnDispose)
+        {
+            return new StopWatcher(actionOnDispose);
+        }
+    }
+
+
+}
