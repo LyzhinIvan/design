768d437fdda4fe99a9f29348405593ce701aafb4
LyzhinIvan <lyzhin-ivan@yandex.ru>
2015-05-02 15:53:21 +0500

    Second task has been solved

---
+++ DIContainer/Commands/HelpCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ninject;
+
+namespace DIContainer.Commands
+{
+    public class HelpCommand : BaseCommand
+    {
+        [Inject]
+        public Lazy<IEnumerable<ICommand>> commandsList { get; set; }
+
+        public HelpCommand()
+        {
+
+        }
+
+        public override void Execute()
+        {
+            foreach (var command in commandsList.Value)
+            {
+                Console.WriteLine(command.Name);
+            }
+        }
+    }
+}
--- DIContainer/Program.cs
+++ DIContainer/Program.cs
@@ -10,20 +10,21 @@ namespace DIContainer
     public class Program
     {
         private readonly CommandLineArgs arguments;
-        private readonly ICommand[] commands;
+        private readonly ICommand[] commands;   
 
         public Program(CommandLineArgs arguments, params ICommand[] commands)
         {
             this.arguments = arguments;
             this.commands = commands;
-        
         }
 
         static void Main(string[] args)
         {
+            //args = new string[] {"help"};
             var container = new StandardKernel();
             container.Bind<ICommand>().To<TimerCommand>();
             container.Bind<ICommand>().To<PrintTimeCommand>();
+            container.Bind<ICommand>().To<HelpCommand>();
             container.Bind<CommandLineArgs>().ToSelf().WithConstructorArgument(args);
             var program = container.Get<Program>();
             program.Run();
